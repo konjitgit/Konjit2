38ca9e16f8321f6c4e21a0da6bcbda7c4de7b4d6
Konjitgit <konjitdev@konjit.com.mx>
2024-01-24 20:51:59 +0300

    Update CORS origin URL

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://konjit2-kjmg.vercel.app",
     // "https://tester-j7yp.vercel.app"
     credentials: true,
   })
