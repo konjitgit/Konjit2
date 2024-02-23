89f8cde071f1a9bf29c8064fdb128f8ee0e8baf2
konjitgit <151919333+konjitgit@users.noreply.github.com>
2024-02-23 21:38:02 +0300

    Update AdminDashboardOrders.jsx

--- frontend/src/pages/AdminDashboardOrders.jsx
+++ frontend/src/pages/AdminDashboardOrders.jsx
@@ -25,9 +25,9 @@ const AdminDashboardOrders = () => {
       minWidth: 130,
       flex: 0.7,
       cellClassName: (params) => {
-        return params.getValue(params.id, "status") === "Delivered"
-          ? "greenColor"
-          : "redColor";
+         return params.value === "Delivered"
+          ? "bg-green-400 text-white"
+          : "bg-pink/30 text-white";
       },
     },
     {
