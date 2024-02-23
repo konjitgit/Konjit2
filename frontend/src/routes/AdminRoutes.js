7630ec5903ec7dea9030386fc8341a51b7f6edda
konjitgit <151919333+konjitgit@users.noreply.github.com>
2024-02-23 21:44:08 +0300

    Update AdminRoutes.js

--- frontend/src/routes/AdminRoutes.js
+++ frontend/src/routes/AdminRoutes.js
@@ -4,11 +4,14 @@ import AdminDashboardSellers from "../pages/AdminDashboardSellers.jsx";
 import AdminDashboardProducts from "../pages/AdminDashboardProducts.jsx";
 import AdminDashboardOrders from "../pages/AdminDashboardOrders.jsx";
 import AdminDashboardRequests from "../pages/AdminDashboardRequests";
+import AdminDashboardComments from "../pages/AdminDashboardComments.jsx";
+
 export {
   AdminDashboardPage,
   AdminDashboardSellers,
   AdminDashboardUsers,
   AdminDashboardProducts,
   AdminDashboardOrders,
-  AdminDashboardRequests,
+  AdminDashboardRequests,AdminDashboardComments,
+
 };
