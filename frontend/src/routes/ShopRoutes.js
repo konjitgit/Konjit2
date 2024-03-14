8b8b953263d2fa1bed9cb7ee3f5b7c877dc5e936
konjitgit <151919333+konjitgit@users.noreply.github.com>
2024-03-14 21:06:58 +0300

    Update ShopRoutes.js

--- frontend/src/routes/ShopRoutes.js
+++ frontend/src/routes/ShopRoutes.js
@@ -4,11 +4,12 @@ import ShopSettingsPage from "../pages/Shop/ShopSettingsPage.jsx";
 import ShopProfilePage from "../pages/Shop/ShopProfilePage";
 import ProfileBuilderPage from "../components/Shop/ProfileBuilder";
 import ShopInboxPage from "../pages/Shop/ShopInboxPage.jsx";
+import ShopOrderDetails from "../pages/Shop/ShopOrderDetails.jsx";
 export {
   ShopCreateProduct,
   ShopProducts,
   ShopSettingsPage,
   ShopProfilePage,
   ProfileBuilderPage,
-  ShopInboxPage,
+  ShopInboxPage,ShopOrderDetails,
 };
