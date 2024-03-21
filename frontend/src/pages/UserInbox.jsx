c8707d67365aade58e3f67e8deac15cd913bcd1c
konjitgit <151919333+konjitgit@users.noreply.github.com>
2024-03-21 11:27:15 +0300

    Update UserInbox.jsx

--- frontend/src/pages/UserInbox.jsx
+++ frontend/src/pages/UserInbox.jsx
@@ -11,7 +11,7 @@ import { TfiGallery } from "react-icons/tfi";
 import styles from "../styles/styles";
 import Avatar from "react-avatar";
 import Loader from "../components/Layout/Loader";
-const ENDPOINT = "https://socket-ongp.onrender.com/";
+const ENDPOINT = "https://socket-9hvq.onrender.com/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
